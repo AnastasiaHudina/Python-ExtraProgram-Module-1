bc9e049bff1eb507834298986a44fb4bb2e4df6b
Anastasia Hudina <90028933+AnastasiaHudina@users.noreply.github.com>
2024-10-27 14:35:45 +0300

    Update task_2.py

--- Лабораторная работа 2/task_2.py
+++ Лабораторная работа 2/task_2.py
@@ -1,3 +1,4 @@
+import math
 salary = 5000  # Ежемесячная зарплата
 spend = 6000  # Траты за первый месяц
 months = 10  # Количество месяцев, которое планируется протянуть без долгов
@@ -12,5 +13,5 @@ for i in range(1, months+1):
     if i > 1:
         spend = spend + (increase * spend)
     money_capital = money_capital + (spend - salary) # записываем в новую переменную сколько не хватило
-
-print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", round(money_capital))
+#print(money_capital)
+print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", math.ceil(money_capital))
