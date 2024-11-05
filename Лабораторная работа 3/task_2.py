c569abb1b356f19ef31a1a9fca3df2405f568d57
Anastasia Hudina <90028933+AnastasiaHudina@users.noreply.github.com>
2024-11-05 23:52:26 +0300

    Update task_2.py

--- Лабораторная работа 3/task_2.py
+++ Лабораторная работа 3/task_2.py
@@ -4,11 +4,7 @@ def find_common_participants(a,b, arg=','):
     second = set(b.split(arg))
     new = list(first.intersection(second))
     return sorted(new)
-
 participants_first_group = "Иванов|Петров|Сидоров"
 participants_second_group = "Петров|Сидоров|Смирнов"
-
-
 # TODO Провеьте работу функции с разделителем отличным от запятой
 print(find_common_participants(a=participants_first_group, b=participants_second_group,arg='|'))
-
